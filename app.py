063e7addaf5741ce3ce9922aed113281fd421b5d
amina-work <mina.works28@gmail.com>
2023-03-17 17:06:58 +0100

    errors

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import pandas as pd
 import numpy as np
-
+import matplotlib.pyplot as plt
 df = pd.read_csv("KDDtrain.csv", header=None)
 print(df.head())
+
+# *********** CHECK IF DATA HAVE NULL VALUES ***********
+print(df.isnull().any())
+
+# *********** LES VALEURS ABBERANTS ***********
+df.plot()
